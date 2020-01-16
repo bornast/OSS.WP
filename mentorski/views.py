bb550a8e1751f66573bf89ca57b57f390d456cfd
bornast <borna.sunjic.st@gmail.com>
2020-01-16 14:41:51 +0100

    added requirements.txt with pip freeze and implemented error messages when u fail to login

--- mentorski/views.py
+++ mentorski/views.py
@@ -17,24 +17,16 @@ def login_page(request):
     context = {
         "form": form
     }
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
+
     if form.is_valid():
         username  = form.cleaned_data.get("email")
         password  = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            try:
-                del request.session['guest_email_id']
-            except:
-                pass
-            if is_safe_url(redirect_path, request.get_host()):
-                return redirect(redirect_path)
-            else:
-                return redirect("/")
+            return redirect("/")
         else:
+            context["login_error"] = "Failed to login, wrong username and/or password!"
             print("Error")
     return render(request, "mentorski/login.html", context)
 
